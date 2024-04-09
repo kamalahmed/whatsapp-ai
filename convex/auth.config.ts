6f677db40f99e7c751d4eadde7da670a7bdead2e
Kamal <kamalacca@gmail.com>
2024-04-09 14:57:16 +0400

    add authentication with clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authconfig =  {
+    providers: [
+      {
+        domain: "https://precious-goat-92.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+export default authconfig;
