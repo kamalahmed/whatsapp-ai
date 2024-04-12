2d4c1f124b27ac499e45a518d0e4ebed86ef61ef
Kamal <kamalacca@gmail.com>
2024-04-12 16:00:28 +0400

    add sedning message and conversation feature

--- src/components/home/message-input.tsx
+++ src/components/home/message-input.tsx
@@ -1,10 +1,29 @@
 import { Laugh, Mic, Plus, Send } from "lucide-react";
 import { Input } from "../ui/input";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Button } from "../ui/button";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../../convex/_generated/api";
+import { useConversationStore } from "@/store/chat-store";
+import toast from "react-hot-toast";
 
 const MessageInput = () => {
 	const [msgText, setMsgText] = useState("");
+	const sendTextMessage = useMutation(api.messages.sendTextMessage);
+	const { selectedConversation } = useConversationStore();
+	const me = useQuery(api.users.getMe);
+
+	const handleTextMessage = async (e: React.FormEvent) => {
+		e.preventDefault();
+
+		try {
+			await sendTextMessage({content: msgText, conversation: selectedConversation!._id, sender: me!._id }); // Here ! means telling ts it will always be available.
+			setMsgText("");
+		} catch (error:any) {
+			toast(error.message);
+		}
+
+	}
 
 	return (
 		<div className='bg-gray-primary p-2 flex gap-4 items-center'>
@@ -13,7 +32,7 @@ const MessageInput = () => {
 				<Laugh className='text-gray-600 dark:text-gray-400' />
 				<Plus className='text-gray-600 dark:text-gray-400' />
 			</div>
-			<form className='w-full flex gap-3'>
+			<form className='w-full flex gap-3' onSubmit={handleTextMessage}>
 				<div className='flex-1'>
 					<Input
 						type='text'
