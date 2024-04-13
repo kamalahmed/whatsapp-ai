4185ebffd40b87cca07204c133e1f4c9a30cfad8
Kamal <kamalacca@gmail.com>
2024-04-13 19:20:50 +0400

    display messages in the chat bubble

--- src/components/home/message-container.tsx
+++ src/components/home/message-container.tsx
@@ -1,12 +1,23 @@
-import { messages } from "@/dummy-data/db";
+// import { messages } from "@/dummy-data/db";
+import { useQuery } from "convex/react";
 import ChatBubble from "./chat-bubble";
+import { useConversationStore } from "@/store/chat-store";
+import { api } from "../../../convex/_generated/api";
 const MessageContainer = () => {
+	const { selectedConversation } = useConversationStore();
+	const messages = useQuery(api.messages.getMessages, {
+		conversation: selectedConversation!._id
+	});
+	const me = useQuery(api.users.getMe);
+	if (!selectedConversation) return null;
+	if (!messages) return null;
+	
 	return (
 		<div className='relative p-3 flex-1 overflow-auto h-full bg-chat-tile-light dark:bg-chat-tile-dark'>
 			<div className='mx-12 flex flex-col gap-3 h-full'>
 				{messages?.map((msg, idx) => (
 					<div key={msg._id}>
-						<ChatBubble />
+						<ChatBubble me={me} message={msg} />
 					</div>
 				))}
 			</div>
