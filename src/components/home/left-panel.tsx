2e397b9e370553453e38331a5610d3d2d84ade03
Kamal <kamalacca@gmail.com>
2024-04-13 20:33:19 +0400

    add loading state management

--- src/components/home/left-panel.tsx
+++ src/components/home/left-panel.tsx
@@ -7,10 +7,22 @@ import { UserButton } from "@clerk/nextjs";
 import UserListDialog from "./user-list-dialog";
 import { useConvexAuth, useQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
+import { useConversationStore } from "@/store/chat-store";
+import { useEffect } from "react";
 
 const LeftPanel = () => {
 	const { isAuthenticated, isLoading } = useConvexAuth();
 	const conversations = useQuery(api.conversations.getMyConversations, isAuthenticated ? undefined : "skip");
+	const {selectedConversation, setSelectedConversation} = useConversationStore();
+
+	useEffect(() => {
+		const conversationIds = conversations?.map((conversation) => conversation._id);
+		if (selectedConversation && conversationIds && !conversationIds.includes(selectedConversation._id)) {
+			setSelectedConversation(null);
+		}
+	}, [conversations, selectedConversation, setSelectedConversation]);
+
+	if(isLoading) return null;
 
 	return (
 		<div className='w-1/4 border-gray-600 border-r'>
